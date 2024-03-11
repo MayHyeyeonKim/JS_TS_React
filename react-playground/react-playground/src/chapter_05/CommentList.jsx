a7d5df225edc2a5a3debc3a244c9021f9aa45b08
MayHyeyeonKim <hkim7963@gmail.com>
2024-03-11 01:16:09 -0700

    comment data들을 별도의 객체로 분리하기 map함수사용

--- react-playground/react-playground/src/chapter_05/CommentList.jsx
+++ react-playground/react-playground/src/chapter_05/CommentList.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import Comment from './Comment'
+import React from "react";
+import Comment from "./Comment";
+
+const comments = [
+  {
+    name: "May Kim",
+    comment: "Hi! This is May",
+  },
+  {
+    name: "Mr.Yoo",
+    comment: "Learning React is fun!",
+  },
+  {
+    name: "Chuchu",
+    comment: "I love you",
+  },
+];
 
 function CommentList(props) {
   return (
     <div>
-        <Comment name={"MayKim"} comment={"This is my first comment in chapter5"} />
-        <Comment name={"Chuchu"} comment={"This is my second comment in chapter5"} />
-
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
-  )
+  );
 }
 
-export default CommentList
+export default CommentList;

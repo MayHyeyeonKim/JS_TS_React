7b51a49dd6076647178f083b5c9294fe077b66f2
MayHyeyeonKim <hkim7963@gmail.com>
2024-05-13 00:48:26 -0700

    prameter & query , useSearchParams

--- REACT_JS/router-example/src/page/Homepage.js
+++ REACT_JS/router-example/src/page/Homepage.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Link} from "react-router-dom";
-import Aboutpage from './Aboutpage';
+import { Link, useNavigate } from "react-router-dom";
+
 const Homepage = () => {
+  const navigate = useNavigate()
+  const goProductPage=()=>{
+    navigate('/products?q=skirts&page=3')
+  };
   return (
     <div>
         <p>Homepage</p>
         <Link to="/about">Go to Aboutpage </Link>
+        <button onClick={goProductPage}> go to product page </button>
     </div>
   )
 }

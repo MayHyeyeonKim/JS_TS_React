63ff662045ce6d020c77d30e22d559a7ab42d7d2
MayHyeyeonKim <hkim7963@gmail.com>
2024-05-27 00:02:52 -0700

    added detail page router

--- REACT_JS/hnm-router-practice/src/page/ProductDetail.js
+++ REACT_JS/hnm-router-practice/src/page/ProductDetail.js
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Button, Dropdown, Alert } from "react-bootstrap";
+import { useParams } from 'react-router-dom'
 
 const ProductDetail = () => {
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const { id } = useParams();
+  const getProductDetail = async()=>{
+    setLoading(true);
+    let url = `http://localhost:8000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    setLoading(false);
+    setProduct(data);
+  }
+  useEffect(()=>{
+    getProductDetail()
+  },[])
+  if (loading || product == null) return <h1>Loading</h1>;
   return (
-    <div>
-        <h1>ProductDetail</h1>
-    </div>
+<Container className="product-detail-card">
+{error ? (
+        <Alert variant="danger" className="text-center">
+          {error}
+        </Alert>
+      ) : (
+  <Row>
+    <Col xs={12} md={6} className="product-detail-img">
+      <img src={product?.img} />
+    </Col>
+    <Col>
+      <div>{product?.title}</div>
+      <div>{product?.price}</div>
+      <div>{product.choice ? "Conscious choice" : ""}</div>
+      <Dropdown className="drop-down">
+      <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                Select Size
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {product?.size.length > 0 &&
+                  product.size.map((item) => (
+                    <Dropdown.Item href="#/action-1">{item}</Dropdown.Item>
+                  ))}
+              </Dropdown.Menu>
+      </Dropdown>
+      <Button variant="dark" className="add-button">
+              Add to cart
+            </Button>
+    </Col>
+  </Row>
+      )}
+</Container>
   )
 }
 
